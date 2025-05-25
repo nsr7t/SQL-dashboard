2e50c9926ac488d09036a9d5c8f3e2fd5a76c7e6
Nasrat <nasratulla2006@gmail.com>
2025-05-25 15:12:21 +0530

    deploy bug

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "/sql-dashboard",
+    base: "/SQL-dashboard",
     plugins: [react(), tailwindcss()],
 
     server: {
